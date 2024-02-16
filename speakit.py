9b040bf4c2351cdae8700c82b87f46a14f75be38
leshec <none>
2024-02-16 14:50:56 +0000

    basic word capture

---
+++ speakit.py
@@ -0,0 +1,31 @@
+import string
+
+def main():
+
+    f = open("data.txt", 'r')
+
+    lines = []
+    words = []
+    try:
+        for line in f:
+            line.translate(str.maketrans('', '', string.punctuation))
+            line.split("\n")
+            lines.append(line)
+        f.close()
+    except FileNotFoundError() as e:
+        raise("file not found xxx", e)
+    finally:
+        f.close()
+
+
+    for line in lines:
+        parts = line.split()
+        for part in parts:
+            if part:
+                part = part.strip()
+                words.append(part)
+
+    print(words[0:150])
+    
+main()
+
